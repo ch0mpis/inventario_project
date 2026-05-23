9ac112cece6906c1e83ab1d5ed2b2aaa1b627f30
Juan <juanpablobuitrago07@gmail.com>
2026-05-22 21:12:21 -0500

    API Testing Completa + Serializer + Viewsets + Seteo de URLs

---
+++ inventario/api/views.py
@@ -0,0 +1,10 @@
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from inventario.models import Producto
+from inventario.api.serializer import ProductoSerializer
+
+class ProductoViewSet(viewsets.ModelViewSet):
+    #permission_classes = [IsAuthenticated]
+    queryset = Producto.objects.all()
+    serializer_class = ProductoSerializer
+    
--- inventario_project/urls.py
+++ inventario_project/urls.py
@@ -14,9 +14,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("<h1>Bienvenido</h1><a href='/api/'>Ir a la API</a>- JUAN PABLO BUITRAGO NARANJO")
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home,),
+    path('api/', include('inventario.api.urls')),
 ]

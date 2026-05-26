2594e8da891bcf7550aa7ef32433b2e08216a480
Juan <juanpablobuitrago07@gmail.com>
2026-05-25 22:45:08 -0500

    feat: Python Decouple para mayor seguridad

--- inventario_project/settings.py
+++ inventario_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,10 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g0m7iax(a-kiqr8v+=n%wbh()8az3jmg0pla3j_s+7cdv4riyb'
+SECRET_KEY = config('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['*']
 

9c74935b71f566c635acb001d21d74a0177e13db
Juan <juanpablobuitrago07@gmail.com>
2026-05-25 20:41:48 -0500

    config: conectar base de datos Render

--- inventario_project/settings.py
+++ inventario_project/settings.py
@@ -76,14 +76,9 @@ WSGI_APPLICATION = 'inventario_project.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'bd_inventario',
-        'USER': 'postgres',
-        'PASSWORD': '123456789',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
 }
 
 

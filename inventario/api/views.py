3ea135dc5b98c9a52862757a2bdd8658d2050ab4
Juan <juanpablobuitrago07@gmail.com>
2026-05-25 22:28:42 -0500

    feat: Filtros de Busqueda API

--- inventario/api/views.py
+++ inventario/api/views.py
@@ -2,9 +2,11 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from inventario.models import Producto
 from inventario.api.serializer import ProductoSerializer
+from django_filters.rest_framework import DjangoFilterBackend
 
 class ProductoViewSet(viewsets.ModelViewSet):
     #permission_classes = [IsAuthenticated]
     queryset = Producto.objects.all()
     serializer_class = ProductoSerializer
-    
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['nombre', 'marca', 'precio']
--- inventario_project/settings.py
+++ inventario_project/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'inventario',
-    'gunicorn',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -133,6 +133,6 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ),
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination','PAGE_SIZE': 10,
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
 }
